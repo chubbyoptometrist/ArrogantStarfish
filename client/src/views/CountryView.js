33ea0a161aa7589a4cbbfd6e9d498958620c3875
Laura Gelston <lauragelston@gmail.com>
2015-11-24 11:19:48 -0800

    Manually merged last PR

--- client/src/views/CountryView.js
+++ client/src/views/CountryView.js
@@ -80,7 +80,7 @@ var CountryView = Backbone.View.extend({
     var context = this;
     return d3.select(element)
       .attr('x', function() {
-        return context.model.get('x');
+        return (context.model.get('x') * 2) / 3;
       })
       .attr('y', function() {
         return context.model.get('y');
@@ -102,7 +102,7 @@ var CountryView = Backbone.View.extend({
       .transition()
       .duration(750)
       .attr({
-        'x': 40,
+        'x': 20,
         'y': 50,
         width: 300,
         height: 300
@@ -124,16 +124,18 @@ var CountryView = Backbone.View.extend({
     var context = this;
     var news = this.model.get('news') || [];
     var charities = this.model.get('charities') || [];
-    //console.log(this)
+    var flag = this.model.get('flag') || '';
+
+    flagCSS = flag !== '' ? 'background-image: url(\'/flags/' + flag + '\')' : '';
     var html = [];
     html[0] = '' +
       '<div class="tooltip-container">' +
-      '  <div class="flag" style="background-image: url(\'/src/img/Libya.png\')"></div>' +
+      '  <div class="flag" style="' + flagCSS + '"></div>' +
       '  <div class="tooltip-title">' +
       '    <p class="tooltip-title-text">' + context.model.get('countryName') + '</p>' +
       '  </div>' +
       '  <div class="tooltip-article">' +
-      '    <div class="tooltip-article-header">What\'s going on rigth now</div>' +
+      '    <div class="tooltip-article-header">What\'s going on right now</div>' +
       '    <hr>' +
       '    <div class="tooltip-article-content">' +
       '      <ul>';
@@ -143,7 +145,7 @@ var CountryView = Backbone.View.extend({
       '    </div>' +
       '  </div>' +
       '  <div>' +
-      '    <div class="tooltip-charity-header">Maybe something here</div>' +
+      '    <div class="tooltip-charity-header">Make the difference</div>' +
       //'    <hr>' +
       '    <div class="tooltip-charity-content">' +
       '      <ul>';
@@ -154,15 +156,21 @@ var CountryView = Backbone.View.extend({
       '  </div>' +
       '</div>';
 
-    news.forEach(function(article) {
-      html[1].push('<li><a href="' + article.url + '">' + article.headline + '</a></li>');
+    news.forEach(function(article, i) {
+      // this line is needed because (i don't know why) but some articles were repeated
+      if (i === 0 || article.headline !== news[i - 1].headline) {
+        html[1].push('<li><a href="' + article.url + '">' + article.headline + '</a></li>');
+      }
     });
-    charities.forEach(function(charity) {
+    charities.forEach(function(charity, i) {
+      console.log(charity)
+      var logo = i === 0 ? '<div class="tooltip-charity-logo" style="background-image: url(' + charity.Logo + ')"></div>' : '';
       var charHtml = '' +
         '<li class="tooltip-charity-box">' +
-        '  <div src="' + charity.Logo + '"></div>' +
-        '  <a href="' + charity.Link + '">' + charity.Name + '</a>' +
+        logo +
+        '  <a class="tooltip-charity-title" href="' + charity.Link + '">' + charity.Name.trim() + '</a>' +
         '  <div class="charity-description">' + charity.Description + '</div>' +
+        '  <div class="charity-more-link"><a href="' + charity.Link + '">Help them >></a></div>' +
         '</li>';
 
       html[3].push(charHtml);
@@ -175,7 +183,7 @@ var CountryView = Backbone.View.extend({
 
   htmlBuilder: function(html) {
     var a = _.reduce(html, function(string, next) {
-      var toJoin = Array.isArray(next) ? next.splice(0, 5).join('') : next
+      var toJoin = Array.isArray(next) ? next.splice(0, 4).join('') : next
       return string + toJoin;
     }, '');
     //console.log(a);
@@ -191,36 +199,5 @@ var CountryView = Backbone.View.extend({
   hideName: function() {
     this.hoverTip.style('display', 'none');
   }
-
-  // showBreakingStory: function() {
-  //   console.log('here');
-  //   var context = this;
-  //   var news = this.model.get('news');
-  //   var html = '' +
-  //     '<div class="breaking-news-container">' +
-  //     '  <div class="country-name">' + this.model.get('countryName') + '</div>';
-  //   news.forEach(function(article) {
-  //     html += '<div class"breaking-news-headline>' + article.headline + '</div>'
-  //   })
-
-  //   var bbox = d3.select(context.el).node().getBBox();
-  //   var data = [{
-  //     x: bbox.x + bbox.width / 2,
-  //     y: bbox.y + bbox.height / 2
-  //   }]
-
-  //   console.log(this.breakingNews);
-  //   this.breakingNews = this.breakingNews || this.selection.append('rect').data(data).attr('class', 'breakingnews');
-  //   this.breakingNews
-  //     .attr({
-  //       fill: 'white',
-  //       width: 100,
-  //       height: 20
-  //     })
-  //     .each(function() {
-  //       return context.positionToCountryCoods(this)
-  //     })
-  //     // .html(html);
-  // }
 
 });
